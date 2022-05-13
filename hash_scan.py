ffb553e034632cc6913d288141c13e855043a7ab
AhmadKasem <amdkhq@gmail.com>
2022-05-13 12:02:13 -0400

    removed commented unnecessarycommented code

--- hash_scan.py
+++ hash_scan.py
@@ -9,53 +9,7 @@ import re
 def main():
 
     p = VirusTotal().scanHash(hashInput())
-    #status codes
-    """ if ("error" in p):
-        if('"code": "WrongCredentialsError"' in p or '"code": "AuthenticationRequiredError"' in p or '"code": "UserNotActiveError"' in p):
-            statusCode = 401
-            print("API FAILED... Status Code: {}".format(statusCode))
-        elif('"code": "BadRequestError"' in p or '"code": "InvalidArgumentError"' in p or '"code": "NotAvailableYet"' in p or
-            '"code": "UnselectiveContentQueryError"' in p or '"code": "UnsupportedContentQueryError"' in p):
-            statusCode = 400
-            print("API FAILED... Status Code: {}".format(statusCode))
-        elif('"code": "ForbiddenError"' in p):
-            statusCode = 403
-            print("API FAILED... Status Code: {}".format(statusCode))
-        elif('"code": "NotFoundError"' in p):
-            statusCode = 404
-            print("API FAILED... Status Code: {}".format(statusCode))
-        elif('"code": "AlreadyExistsError"' in p):
-            statusCode = 409
-            print("API FAILED... Status Code: {}".format(statusCode))
-        elif('"code": "FailedDependencyError"' in p):
-            statusCode = 424
-            print("API FAILED... Status Code: {}".format(statusCode))
-        elif('"code": "QuotaExceededError"' in p or '"code": "TooManyRequestsError"' in p):
-            statusCode = 429
-            print("API FAILED... Status Code: {}".format(statusCode))
-        elif('"code": "TransientError"' in p):
-            statusCode = 503
-            print("API FAILED... Status Code: {}".format(statusCode))
-        elif('"code": "DeadlineExceededError"' in p):
-            statusCode = 504
-            print("API FAILED... Status Code: {}".format(statusCode))
-    elif('"data":' in p):
-        statusCode = 200
-        print("Status Code: {}".format(statusCode))
-
-    #how many AV's detected the file
-    numOfMalicious = re.search(r'("malicious":) \d+[,]',p )
-    strMalicious = (re.search(r'\d+', numOfMalicious.group(0)))
-    intMalicicous = int(strMalicious.group(0))
-
-    if(intMalicicous > 5):
-        print("{} AV engines detected the file".format(intMalicicous))
-    elif(intMalicicous < 5):
-        print("The file may be malicious. {} AV engines deteccted the file".format(intMalicicous))
-    elif(intMalicicous == 0):
-        print("The file is clean")
-        #print(p) """
-
+    
 #calling api
 class VirusTotal:
     def __init__(self):
@@ -70,7 +24,8 @@ class VirusTotal:
             }
         self.response = requests.get(url, headers=self.headers)
         scanResult = self.response.text
-        ####
+        
+        #status codes
         if ("error" in scanResult):
             if('"code": "WrongCredentialsError"' in scanResult or '"code": "AuthenticationRequiredError"' in scanResult or '"code": "UserNotActiveError"' in scanResult):
                 statusCode = 401
